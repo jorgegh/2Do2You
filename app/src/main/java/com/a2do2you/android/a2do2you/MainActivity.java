48015c8774df1d1b6b919538428d46aa4bd02114
Daniel <hmartindaniel@gmail.com>
2017-06-15 23:13:13 +0200

    Comento esta mierda 2

--- app/src/main/java/com/a2do2you/android/a2do2you/MainActivity.java
+++ app/src/main/java/com/a2do2you/android/a2do2you/MainActivity.java
@@ -186,7 +186,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tarea.setDescripcion(cursor.getString(2));
         tarea.setEstado(cursor.getInt(3));
         tarea.setTipo(cursor.getInt(4));
-        //tarea.setSubtarea(cursor.getInt(5)); commento esto
+        //tarea.setSubtarea(cursor.getInt(5)); commento esto otra vez
         tareas.add(tarea);
     }
 
